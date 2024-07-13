43aeff111ff81d47403d434f05054ca85325bca1
Rituraj Singh <117657890+RiturajS12@users.noreply.github.com>
2024-07-13 19:59:37 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,18 +1,25 @@
 import socket
 from threading import Thread
 
+
 server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-server.bind(("localhost",5550))
+server.bind(("192.168.57.132",5550))
 
 server.listen()
 all_clients = {}
 
 def client_thread(client):
     while True:
-        msg = client.recv(1024)
-        for c in all_clients:
-            c.send(msg)
-
+        try:    
+            msg = client.recv(1024)
+            for c in all_clients:
+                c.send(msg)
+        except:
+            for c in all_clients:
+                if c != client:
+                    c.send(f"{name} has left the room!".encode())
+            del all_clients[client]
+            break
 
 while True:
     print("server is starting...")
@@ -20,5 +27,8 @@ while True:
     print("connected")
     name =  client.recv(1024).decode()
     all_clients[client] = name
+    for c in all_clients:
+        if c != client:
+            c.send(f"{name} has joined the room!".encode())
     thread = Thread(target=client_thread,args=(client,))
     thread.start()
